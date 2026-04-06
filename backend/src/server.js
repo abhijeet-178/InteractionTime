1b092d4562300193b3f8fa08b8b3df77bb6c86a5
Abhijeet jha <abhijeetjha9431@gmail.com>
2026-04-06 21:14:11 +0530

    fix+

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,9 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 
-app.use(cors());
+app.use(cors({
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
