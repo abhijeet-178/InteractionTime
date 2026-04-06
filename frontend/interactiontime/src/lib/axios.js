b891015f9361fe7a8d8e6378de8462145db7e033
Abhijeet jha <abhijeetjha9431@gmail.com>
2026-04-06 17:38:18 +0530

    Fix case sensitivity in component name

--- frontend/interactiontime/src/lib/axios.js
+++ frontend/interactiontime/src/lib/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+const backendUrl=process.env.REACT_APP_BACKEND_URL || "http://localhost:5001/api";
 export const axiosInstance=axios.create({
     baseURL:"http://localhost:5001/api",
     withCredentials:true,
